1b8f9d510a3ec0e1429acec60d19a056532a0fb8
Kraus Ondrej <klabzub@seznam.cz>
2020-02-26 11:27:26 +0100

    Submitted solution

--- JumpingPlatformGame/Entities.cs
+++ JumpingPlatformGame/Entities.cs
@@ -3,13 +3,53 @@
 namespace JumpingPlatformGame {
 	class Entity {
 		public virtual Color Color => Color.Black;
+        public WorldPoint Location;
+
+        public virtual void Update(Seconds seconds) {}
 	}
 
-	class MovableEntity : Entity {
-	}
+    class MovableEntity : Entity
+    {
+        public Movement Horizontal = new Movement();
+        public override void Update(Seconds seconds)
+        {
+            var newLocation = this.Location.X + this.Horizontal.Speed * seconds;
 
-	class MovableJumpingEntity : MovableEntity {
-	}
+            if (newLocation.Value > this.Horizontal.UpperBound.Value)
+            {
+                newLocation = this.Horizontal.UpperBound;
+                this.Horizontal.ReverseDirection();
+            }
+            if (newLocation.Value < this.Horizontal.LowerBound.Value)
+            {
+                newLocation = this.Horizontal.LowerBound;
+                this.Horizontal.ReverseDirection();
+            }
+
+            this.Location.X = newLocation;
+        }
+    }
+
+    class MovableJumpingEntity : MovableEntity {
+        public Movement Vertical = new Movement();
+        public override void Update(Seconds seconds)
+        {
+            base.Update(seconds);            
+            var newLocation = this.Location.Y + (this.Vertical.Speed * seconds);
+
+            if (newLocation.Value > this.Vertical.UpperBound.Value)
+            {
+                newLocation = this.Vertical.UpperBound;
+                this.Vertical.ReverseDirection();
+            }
+            if (newLocation.Value < this.Vertical.LowerBound.Value)
+            {
+                newLocation = this.Vertical.LowerBound;               
+            }
+
+            this.Location.Y = newLocation;
+        }
+    }
 
 	class Joe : MovableEntity {
 		public override string ToString() => "Joe";
@@ -30,5 +70,4 @@ namespace JumpingPlatformGame {
 		public override string ToString() => "Jill";
 		public override Color Color => Color.Pink;
 	}
-
 }
--- JumpingPlatformGame/Physics.cs
+++ JumpingPlatformGame/Physics.cs
@@ -1,3 +1,79 @@
 ﻿namespace JumpingPlatformGame {
-	
+    public struct Movement
+    {
+        public Meters UpperBound { get; set; }
+        public Meters LowerBound { get; set; }
+        public MeterPerSeconds Speed;
+       
+        public void ReverseDirection()
+        {
+            Speed.Value = Speed.Value * -1;
+        }
+    }
+
+    public struct WorldPoint
+    {
+        public Meters X, Y;
+    }
+    public struct MeterPerSeconds
+    {
+        public double Value { get; set; }
+        
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public static Meters operator *(MeterPerSeconds mps, Seconds s) => new Meters() { Value = mps.Value * s.Value};
+
+    }
+    public struct Meters
+    {
+        public double Value { get; set; }
+
+        public static Meters operator *(Meters x, int direction) => new Meters() { Value = x.Value *direction };
+        public static MeterPerSeconds operator /(Meters m, Seconds s)
+        {
+            double v = m.Value / s.Value;
+            return new MeterPerSeconds() { Value = v };
+        }
+        public static Meters operator +(Meters x, Meters y) => new Meters() { Value = x.Value + y.Value };
+    }
+    public struct Seconds
+    {
+        public double Value { get; set; }
+    }
+    public static class IntExtensions
+    {
+        public static Seconds Seconds(this int value)
+        {
+            return new Seconds() { Value = value };
+        }
+        public static Meters Meters(this int value)
+        {
+            return new Meters() { Value = value };
+        }
+        public static MeterPerSeconds MeterPerSeconds(this int value)
+        {
+            return new MeterPerSeconds() { Value = value };
+        }
+    }
+
+    public static class DoubleExtensions
+    {
+        public static Seconds Seconds(this double value)
+        {
+            return new Seconds() { Value = value };
+        }
+        public static Meters Meters(this double value)
+        {
+            return new Meters() { Value = value };
+        }
+        public static MeterPerSeconds MeterPerSeconds(this double value)
+        {
+            return new MeterPerSeconds() { Value = value };
+        }
+    }
+
+
 }
